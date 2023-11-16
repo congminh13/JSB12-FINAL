76c561b56f3de16a8b0d7c856666f5acea421e38
congminh13 <145847608+congminh13@users.noreply.github.com>
2023-11-16 19:07:26 +0700

    Update allow_checker.js
    update verification

--- allow_checker.js
+++ allow_checker.js
@@ -1,11 +1,11 @@
 let permit= JSON.parse(localStorage.getItem('GLOBAL_LOG_STATE'));
 console.log(permit.state);
 
+let formAll = document.getElementById('req-log');
+
 if (permit.state == 'allowed') {
     console.log('login successful!');
     let data = JSON.parse(localStorage.getItem('USER_INFO'));
     document.getElementById("user-log-name").innerHTML = data.name;
-} else {
-    let formAll = document.getElementById('req-log');
-    formAll.classList.remove("hidden-content");
+    formAll.classList.add("hidden-content");
 }
